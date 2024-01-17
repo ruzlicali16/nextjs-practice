d12b59be7dc4567b9098e6a632b365f300c0477d
ruzlicali16 <ruzlicali24@gmail.com>
2024-01-17 22:00:43 +0800

    resolve error in building

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -20,7 +20,6 @@ export default async function InvoicePage({
     query?: string;
     page?: string;
   };
-  pathname: string;
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
