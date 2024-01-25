65e179c6ec6faded18f0c705a4bf06d1b539e143
ruzlicali16 <ruzlicali24@gmail.com>
2024-01-25 14:27:28 +0800

    try to fix error in build

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,26 +1,19 @@
+import { fetchCustomers } from "@/app/lib/data";
 import { CustomerField } from "@/app/lib/definitions";
 import { BASE_API_URL } from "@/app/lib/utils";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/create-form";
 
-const API_URL = /development/i.test(process.env.NODE_ENV) ? BASE_API_URL : '';
-const url = `${API_URL}/api/dashboard/customers`;
-const getCustomers = async () => {
-    const res = await fetch(`${url}/customers`,
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-    return await res.json() as CustomerField[];
-}
-
 export default async function InvoicePage() {
-    if (!BASE_API_URL) {
-        return null;
-    }
-    const customers = await getCustomers();
+    // const API_URL = /development/i.test(process.env.NODE_ENV) ? BASE_API_URL : '';
+    // const url = `${API_URL}/api/dashboard/customers`;
+    // const response = await fetch(url, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    // });
+    // const customers = await response.json() as CustomerField[];
 
     return (
         <main>
@@ -33,7 +26,7 @@ export default async function InvoicePage() {
                         active: true
                     }
                 ]} />
-            <Form customers={customers} />
+            <Form customers={[]} />
         </main>
     );
 }
